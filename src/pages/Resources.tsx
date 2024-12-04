17cbf15fe407214fec9f9609ccb74fd08f16279f
SawB06 <157073273+SawB06@users.noreply.github.com>
2024-12-03 19:14:32 -0500

    Update Resources.tsx

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -146,6 +146,7 @@ const Resources: React.FC = () => {
           { name: "Attendance", path: "/attendance" },
           { name: "Resources", path: "/resources" },
           { name: "Homework", path: "/homework" },
+          { name: "Discussion", path: "/discussion"}
           
         ]}
         />
